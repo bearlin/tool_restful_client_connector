cc9985ed30c0f8dd76b4a283c108c7bc980c4969
BearLin <bear.lin.001@gmail.com>
2016-04-16 12:47:33 +0800

    1. Add x64/x86 build selection. 2. Fix -Werror

--- build.sh
+++ build.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+usage() {
+  echo "Usage:"
+  echo "./build.sh <ARCH_TYPE>"
+  echo "ARCH_TYPE: [x86|x64]"
+}
+
+if [ "$#" -lt 1 ]; then
+  usage
+  exit
+fi
+
 # remove Build/ for a clean build
+ARCH_TYPE="x64"
 for argument in $@
 do
-  if [ "$argument" == "clean" ]; then
-    rm -rf Build
+  if [ "$argument" == "x86" ]; then
+    ARCH_TYPE="x86"
+  fi
+  if [ "$argument" == "x64" ]; then
+    ARCH_TYPE="x64"
   fi
 done
 
 # build
 #mkdir -p Build && cd Build && rm -rf && cmake .. && make && cd -
-mkdir -p Build && cd Build && rm -rf && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && cd -
+mkdir -p Build && cd Build && rm -rf && cmake -DArchType=${ARCH_TYPE} -DCMAKE_BUILD_TYPE=Debug .. && make && cd -
